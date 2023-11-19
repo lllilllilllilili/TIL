6898a478decaa907d4f563ee0b5160d6ce5c1dd3
lllilllilllilili <hik3562@hufs.ac.kr>
2023-11-19 20:16:24 +0900

    add | threadLocal 테스트코드 추가

---
+++ advanced/src/test/java/hello/advanced/trace/threadLocal/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadLocal;
+
+import hello.advanced.trace.threadLocal.code.FieldService;
+import hello.advanced.trace.threadLocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        //sleep(2000); //동시성 문제가 발생 안함
+        sleep(100);
+        threadB.start();
+
+        sleep(2000);
+        log.info("main exit");
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
