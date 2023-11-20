b4b545dab131e5c1d01b2c534211abab9bbe7e9c
lllilllilllilili <hik3562@hufs.ac.kr>
2023-11-20 19:39:32 +0900

    init: catalog service project init

---
+++ catalog-service/src/main/data.sql
@@ -0,0 +1,10 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-001', 'Berlin', 100, 1500);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-002', 'Tokyo', 101, 1501);
+
+insert into catalog(product_id, product_name, stock, unit_price)
+values('CATALOG-003', 'Stockholm', 102, 1502);
+
+
