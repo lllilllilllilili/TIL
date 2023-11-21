77ecb9b2a13479e4970111b06cc2ba25fb819e5b
lllilllilllilili <hik3562@hufs.ac.kr>
2023-11-21 14:18:56 +0900

    add: Spring Order Service 추가

--- spring/advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
+++ spring/advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class OrderRepositoryV0 {
 
-    public void save(String itemId) throws InterruptedException {
+    public void save(String itemId) {
         //저장 로직
         if (itemId.equals("ex")) {
             throw new IllegalStateException("예외 발생!");
@@ -17,7 +17,11 @@ public class OrderRepositoryV0 {
 
     }
 
-    private void sleep(int millis) throws InterruptedException {
-        Thread.sleep(millis);
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
