b963000a2f502ce521294013a8704deb0f9fda8f
lllilllilllilili <hik3562@hufs.ac.kr>
2024-01-09 14:52:02 +0900

    add | exception에 대해서도 test코드 수정

--- advanced/src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
+++ advanced/src/test/java/hello/advanced/trace/hellotrace/HelloTraceV2Test.java
@@ -18,7 +18,9 @@ class HelloTraceV2Test {
     @Test
     void begin_exception() {
         HelloTraceV2 trace = new HelloTraceV2();
-        TraceStatus status = trace.begin("hello");
-        trace.exception(status, new IllegalStateException());
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.beginSync(status1.getTraceId(), "hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
     }
 }
