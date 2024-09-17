adb1d7a1c6fef9014f5b84e3702d4066f85e5287
lllilllilllilili <hik3562@hufs.ac.kr>
2024-09-17 17:09:25 +0900

    Spring | Account Controller 테스트코드 추가

--- test/src/test/java/com/example/test/TestApplicationTests.java
+++ test/src/test/java/com/example/test/TestApplicationTests.java
@@ -1,11 +1,28 @@
 package com.example.test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TestApplicationTests {
+
+    @Autowired private MockMvc mockMvc;
+
+    @DisplayName("회원 가입 화면 보여지는지 테스트")
     @Test
-    void contextLoads() {
+    void signUpForm() throws Exception{
+        mockMvc.perform(get("/sign-up"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("account/sign-up"));
     }
+
 }
