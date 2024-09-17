ba37da0badc1678f1a7086a3f9f52e8626af8c41
lllilllilllilili <hik3562@hufs.ac.kr>
2024-09-17 17:10:22 +0900

    Spring | test 코드 수정

--- test/src/test/java/com/example/test/TestApplicationTests.java
+++ test/src/test/java/com/example/test/TestApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -21,6 +22,7 @@ class TestApplicationTests {
     @Test
     void signUpForm() throws Exception{
         mockMvc.perform(get("/sign-up"))
+            .andDo(print())
             .andExpect(status().isOk())
             .andExpect(view().name("account/sign-up"));
     }
