ec5851e29b8cb541768adcadde9c0a3ca8350882
lllilllilllilili <hik3562@hufs.ac.kr>
2024-09-21 21:06:34 +0900

    Spring | SignUpFormValidator 추가 - InitBinder 로 Validators 코드 작성

--- test/src/main/java/com/example/test/account/AccountController.java
+++ test/src/main/java/com/example/test/account/AccountController.java
@@ -1,16 +1,29 @@
 package com.example.test.account;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class AccountController  implements AccountControllerDocs  {
+
+    private final SignUpFormValidator signUpFormValidator;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+
     @GetMapping("/sign-up")
     public String signUpForm(String userId) {
         return "account/sign-up"; //처리 방법에 대해 고민
@@ -22,6 +35,9 @@ public class AccountController  implements AccountControllerDocs  {
             log.info("signForm not valid");
             return "account/sign-up";
         }
+
+        //signUpFormValidator.validate(signUpForm, errors);
+
         log.info("signForm valid");
         return "redirect:/";
     }
