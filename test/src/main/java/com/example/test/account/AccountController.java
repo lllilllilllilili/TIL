d0f1e89fe20bcac495747380b7093452fbd697f7
lllilllilllilili <hik3562@hufs.ac.kr>
2024-09-21 20:00:45 +0900

    Spring | form 이 없으면 첫페이지로 redirect

--- test/src/main/java/com/example/test/account/AccountController.java
+++ test/src/main/java/com/example/test/account/AccountController.java
@@ -1,13 +1,24 @@
 package com.example.test.account;
 
 import ch.qos.logback.core.model.Model;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController  implements AccountControllerDocs  {
     @GetMapping("/sign-up")
-    public String signUpForm(SignUpForm signUpForm) {
+    public String signUpForm(String userId) {
         return "account/sign-up"; //처리 방법에 대해 고민
+    }
+
+    @PostMapping("sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+        return "redirect:/";
     }
 }
